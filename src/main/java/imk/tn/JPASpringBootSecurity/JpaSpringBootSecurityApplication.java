3bbe71d0ad522f339c231c246e811e4bec53a6bc
IMK <imk.med5@gmail.com>
2020-04-17 10:18:40 +0300

    build project

---
+++ src/main/java/imk/tn/JPASpringBootSecurity/JpaSpringBootSecurityApplication.java
@@ -0,0 +1,13 @@
+package imk.tn.JPASpringBootSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaSpringBootSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaSpringBootSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/imk/tn/JPASpringBootSecurity/JpaSpringBootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package imk.tn.JPASpringBootSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaSpringBootSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
